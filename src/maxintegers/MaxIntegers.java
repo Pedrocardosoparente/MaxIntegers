4240024faa749fa9192d47dd1f8f71f2c03b3d6a
Aluno <Aluno@10.85.198.36>
2024-06-19 16:30:31 -0300

    MAX

---
+++ src/maxintegers/MaxIntegers.java
@@ -0,0 +1,18 @@
+public class MaxIntegers {
+
+    
+public static void main(String[] args) {
+int num1 = 3, num2 = 3, num3 = 3;
+int max = 0;
+if (num1 > max) {
+max = num1;
+}//fim if
+if (num2 > max) {
+max = num2;
+}//fim if
+if (num3 > max) {
+max = num3;
+}//fim if
+System.out.println("O máximo de 3 números é " + max);
+}
+}
